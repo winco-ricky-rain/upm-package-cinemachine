91e633c244a823f6c72dd556dcf52c956a7f0b35
Greg Labute <gregoryl@unity3d.com>
2018-11-22 12:07:20 -0500

    here are the first Unit Tests!

--- Tests/Editor/MathHelpersTest.cs
+++ Tests/Editor/MathHelpersTest.cs
@@ -2,22 +2,102 @@
 using UnityEngine.TestTools;
 using NUnit.Framework;
 using System.Collections;
-using NUnit.Framework;
-//using static Unity.Mathematics.math;
+using Unity.Mathematics;
 
 namespace Cinemachine.ECS
 {
     [TestFixture]
     public class MathHelpersTest 
     {
-
-	    [Test]
-	    public void PlayModeSampleTestSimplePasses() 
+        [Test]
+	    public void AlmostZero() 
         {
-		    // Use the Assert class to test conditions.
+            Assert.That(new float3(0, 0, 0).AlmostZero());
+            Assert.That(new float3(0, 0, -0.0001f).AlmostZero());
+            Assert.That(new float3(-0.0001f, 0.0001f, -0.0001f).AlmostZero());
+
+            Assert.That(!new float3(0, 0, 0.001f).AlmostZero());
+            Assert.That(!new float3(-0.001f, 0.001f, 0.001f).AlmostZero());
+            Assert.That(!new float3(0.1f, 0.01f, 0).AlmostZero());
+        }
+
+        [Test]
+	    public void ProjectOntoPlane() 
+        {
+            var d1 = math.normalize(new float3(1, 2, 3));
+            var d2 = math.normalize(new float3(2, 3, 1));
+            Assert.That(MathHelpers.ProjectOntoPlane(d1, d1).AlmostZero());
+            var v = MathHelpers.ProjectOntoPlane(d1, d2);
+            Assert.That(!v.AlmostZero());
+            Assert.That(math.length(v) < 1);
+            Assert.That(math.abs(math.dot(v, d2)) < MathHelpers.Epsilon);
+            Assert.That(!math.cross(v, d2).AlmostZero());
+        }
+
+        [Test]
+	    public void DampFloat() 
+        {
+            const float dampTime = 10f;
+            const float initial = 100f;
+            float[] fixedFactor = new float[3] { 0.79f, 0f, 1.07f };
+            for (int f = 0; f < fixedFactor.Length; ++f)
+            {
+                float t = 0;
+                float r = MathHelpers.Damp(initial, dampTime, t);
+                Assert.AreEqual(0, r);
+                Assert.Less(r, initial);
+                const int iterations = 10;
+                for (int i = 0; i < iterations; ++i)
+                {
+                    t += dampTime / iterations;
+                    float fdt = fixedFactor[f] * t;
+                    string msg = "i == " + i + ", t == " + t + ", fdt = " + fdt;
+                    if (i != iterations-1)
+                        Assert.Less(t, dampTime, msg);
+                    else
+                        t = dampTime;
+                    float r2 = MathHelpers.Damp(initial, dampTime, t, fdt);
+                    Assert.Less(r, r2, msg);
+                    r = r2;
+                }
+                Assert.AreEqual(r, initial);
+            }
 	    }
 
-/*
+        [Test]
+	    public void DampFloat3() 
+        {
+            float3 dampTime = new float3(10f, 9f, 8f);
+            float3 initial = new float3(100f, 110f, 120f);
+            float[] fixedFactor = new float[3] { 0.79f, 0f, 1.07f };
+            for (int f = 0; f < fixedFactor.Length; ++f)
+            {
+                float t = 0;
+                float3 r = MathHelpers.Damp(initial, dampTime, t);
+                Assert.AreEqual(r.x, 0);
+                Assert.AreEqual(r.y, 0);
+                Assert.AreEqual(r.z, 0);
+                const int iterations = 10;
+                for (int i = 0; i < iterations; ++i)
+                {
+                    t += math.cmax(dampTime) / iterations;
+                    float fdt = fixedFactor[f] * t;
+                    string msg = "i == " + i + ", t == " + t + ", fdt = " + fdt;
+                    if (i != iterations-1)
+                        Assert.Less(t, math.cmax(dampTime), msg);
+                    else
+                        t = math.cmax(dampTime);
+                    float3 r2 = MathHelpers.Damp(initial, dampTime, t, fdt);
+                    Assert.LessOrEqual(r.x, r2.x, msg);
+                    Assert.LessOrEqual(r.y, r2.y, msg);
+                    Assert.LessOrEqual(r.z, r2.z, msg);
+                    r = r2;
+                }
+                Assert.AreEqual(r, initial);
+            }
+	    }
+
+#if false
 	    // A UnityTest behaves like a coroutine in PlayMode
 	    // and allows you to yield null to skip a frame in EditMode
 	    [UnityTest]
@@ -27,7 +107,7 @@ namespace Cinemachine.ECS
 		    // yield to skip a frame
 		    yield return null;
 	    }
-*/
+#endif
     }
 }
 
