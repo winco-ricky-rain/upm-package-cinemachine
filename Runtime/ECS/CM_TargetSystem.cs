6466735e1f13e46bbad842bd0cbea8f2de356f4e
Greg Labute <gregoryl@unity3d.com>
2019-01-30 13:57:42 -0500

    Added target group update

--- Runtime/ECS/CM_TargetSystem.cs
+++ Runtime/ECS/CM_TargetSystem.cs
@@ -15,11 +15,20 @@ namespace Cinemachine.ECS
         public float radius;
     }
 
+    [Serializable]
+    [InternalBufferCapacity(4)]
+    public struct CM_GroupBufferElement : IBufferElementData
+    {
+        public Entity target;
+        public float weight;
+    }
+
     [ExecuteAlways]
     [UpdateAfter(typeof(EndFrameTransformSystem))]
     public class CM_TargetSystem : JobComponentSystem
     {
         ComponentGroup m_mainGroup;
+        ComponentGroup m_groupGroup;
 
         public struct TargetInfo
         {
@@ -34,6 +43,10 @@ namespace Cinemachine.ECS
             m_mainGroup = GetComponentGroup(
                 ComponentType.ReadOnly<CM_Target>(),
                 ComponentType.ReadOnly<LocalToWorld>());
+
+            m_groupGroup = GetComponentGroup(
+                ComponentType.Create<CM_Target>(),
+                ComponentType.ReadOnly(typeof(CM_GroupBufferElement)));
 
             m_targetLookup = new NativeHashMap<Entity, TargetInfo>(64, Allocator.Persistent);
         }
@@ -63,6 +76,79 @@ namespace Cinemachine.ECS
             }
         }
 
+        [BurstCompile]
+        struct UpdateGroups : IJobParallelFor
+        {
+            [ReadOnly] public BufferArray<CM_GroupBufferElement> groupBuffers;
+            [ReadOnly] public NativeHashMap<Entity, TargetInfo> hashMap;
+            public NativeArray<TargetInfo> infoArray;
+
+            public void Execute(int index)
+            {
+                var buffer = groupBuffers[index];
+
+                int numTargets = 0;
+                float3 avgPos = float3.zero;
+                float avgWeight = 0;
+                for (int i = 0; i < buffer.Length; ++i)
+                {
+                    var b = buffer[i];
+                    if (hashMap.TryGetValue(b.target, out TargetInfo item))
+                    {
+                        ++numTargets;
+                        avgPos += item.position * b.weight;
+                        avgWeight += b.weight;
+                    }
+                }
+
+                // This is a very approximate implementation
+                if (numTargets > 0 && avgWeight > 0.001f)
+                {
+                    avgPos /= avgWeight;
+                    avgWeight /= numTargets;
+                    numTargets = 0;
+                    float3 minPos = float3.zero;
+                    float3 maxPos = float3.zero;
+                    for (int i = 0; i < buffer.Length; ++i)
+                    {
+                        var b = buffer[i];
+                        if (hashMap.TryGetValue(b.target, out TargetInfo item))
+                        {
+                            float w = math.max(1, b.weight / avgWeight);
+                            float3 p = math.lerp(avgPos, item.position, w);
+                            float3 r = math.lerp(0, item.radius, w) * new float3(1, 1, 1);
+                            float3 p0 = p - r;
+                            float3 p1 = p + r;
+                            minPos = math.select(math.min(minPos, p0), p0, numTargets == 0);
+                            maxPos = math.select(math.min(maxPos, p1), p1, numTargets == 0);
+                            ++numTargets;
+                        }
+                        infoArray[index] = new TargetInfo
+                        {
+                            position = (minPos + maxPos) / 2,
+                            radius = math.length(maxPos - minPos) / 2,
+                            rotation = quaternion.identity
+                        };
+                    }
+                }
+            }
+        }
+
+        [BurstCompile]
+        struct SetGroupInfo : IJobParallelFor
+        {
+            [ReadOnly] public EntityArray entities;
+            [ReadOnly] [DeallocateOnJobCompletion] public NativeArray<TargetInfo> infoArray;
+            public NativeHashMap<Entity, TargetInfo>.Concurrent hashMap;
+            public ComponentDataArray<CM_Target> targets;
+
+            public void Execute(int index)
+            {
+                hashMap.TryAdd(entities[index], infoArray[index]);
+                targets[index] = new CM_Target { radius = infoArray[index].radius };
+            }
+        }
+
         protected override JobHandle OnUpdate(JobHandle inputDeps)
         {
             // Make sure all readers have finished with the table
@@ -70,8 +156,9 @@ namespace Cinemachine.ECS
             TargetTableReadJobHandle = default(JobHandle);
 
             var objectCount = m_mainGroup.CalculateLength();
+            var groupCount = m_groupGroup == null ? 0 : m_groupGroup.CalculateLength();
             m_targetLookup.Clear();
-            m_targetLookup.Capacity = math.max(m_targetLookup.Capacity, objectCount);
+            m_targetLookup.Capacity = math.max(m_targetLookup.Capacity, objectCount + groupCount);
 
             var hashJob = new HashTargets()
             {
@@ -81,6 +168,27 @@ namespace Cinemachine.ECS
                 hashMap = m_targetLookup.ToConcurrent()
             };
             TargetTableWriteHandle = hashJob.Schedule(objectCount, 32, inputDeps);
+
+            if (groupCount > 0)
+            {
+                var infoArray = new NativeArray<TargetInfo>(groupCount, Allocator.TempJob);
+                var groupJob = new UpdateGroups
+                {
+                    groupBuffers = m_groupGroup.GetBufferArray<CM_GroupBufferElement>(),
+                    hashMap = m_targetLookup,
+                    infoArray = infoArray
+                };
+                TargetTableWriteHandle = groupJob.Schedule(groupCount, 32, TargetTableWriteHandle);
+
+                var setGroupsJob = new SetGroupInfo
+                {
+                    entities = m_groupGroup.GetEntityArray(),
+                    infoArray = infoArray,
+                    hashMap = m_targetLookup.ToConcurrent(),
+                    targets = m_groupGroup.GetComponentDataArray<CM_Target>()
+                };
+                TargetTableWriteHandle = setGroupsJob.Schedule(groupCount, 32, TargetTableWriteHandle);
+            }
             return TargetTableWriteHandle;
         }
 
---
+++ Runtime/ECS_Hybrid/Components/CM_GroupComponent.cs
@@ -0,0 +1,9 @@
+using Unity.Entities;
+using Cinemachine.ECS;
+
+namespace Cinemachine.ECS_Hybrid
+{
+    [UnityEngine.DisallowMultipleComponent]
+    [SaveDuringPlay]
+    public class CM_GroupComponent : DynamicBufferProxy<CM_GroupBufferElement> { }
+}
